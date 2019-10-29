813c4821c516f92f2324476c8bb26c54d6416a10
emperorlu <1021779409@qq.com>
2019-10-29 20:12:01 +0800

    aep

--- src/aep_system.cc
+++ src/aep_system.cc
@@ -427,7 +427,7 @@ string aepsystem::Get(const std::string& key)
             {
                 // cout << "[DEBUG] Read Cache!" << endl;
                 
-                Read_Cache();
+                // Read_Cache();
             }
             switch (id)
             {
