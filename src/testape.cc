7aea50502e9bdc9a00c05ef252d58bd29284c945
emperorlu <1021779409@qq.com>
2019-10-30 16:16:21 +0800

    aep

--- src/testape.cc
+++ src/testape.cc
@@ -14,21 +14,22 @@ int main()
     char m_key[10][NVM_KeyBuf];
     for(int i = 0; i < 10; i++) 
     {   
-        snprintf(keybuf, sizeof(keybuf), "%08d", i);
+        snprintf(keybuf, sizeof(keybuf), "%07d", i);
         string data(keybuf, KEY_SIZE);
         char keybuf[NVM_KeyBuf + 1];
         char sign[NVM_SignSize + 1];
         memcpy(keybuf, data.c_str(), data.size());
         snprintf(sign, sizeof(sign), "%07d", 1000000);
         string signdata(sign, NVM_SignSize);
-        memcpy(keybuf + NVM_KeySize + NVM_PointSize, signdata.c_str(), NVM_SignSize);
         string tmp_key(keybuf, NVM_KeyBuf);
         memcpy(m_key[i], tmp_key.c_str(), NVM_KeyBuf);
-        cout << "before m_key[i]: " << m_key[i] << endl;
+        int len = sizeof(m_key[i]);
+        printf("before m_key[i]: %s", m_key[i]);
         string tmp (m_key[i], NVM_KeyBuf);
+
+
         cout << "before insert Key: " << tmp << endl;
-        // int len = sizeof(m_key[i]);
-        int len = tmp.length();
+        len = tmp.length();
         int hot = stoi(tmp.substr(len-6));
         cout << "before hot: " << hot << endl;
         for(int j = 0; j < ops; j++)
@@ -36,7 +37,6 @@ int main()
             hot++;
             if(j>ops/8){
                 if(tmp[len-8] == '1'){
-                    cout << "1" << endl;
                     tmp[len-8] = '0';
                     j = ops;
                 }
@@ -48,7 +48,7 @@ int main()
         cout << "after insert Key: " << tmp << endl;
         cout << "after hot: " << hot << endl;
         memcpy(m_key[i], tmp.c_str(), NVM_KeyBuf);
-        cout << "after m_key[i]: " << m_key[i] << endl;
+        printf("after m_key[i]: %s", m_key[i]);
     }
     
 }
