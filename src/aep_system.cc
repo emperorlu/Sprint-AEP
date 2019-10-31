1c231e10b71c5168b3b0e6e3f0266b7f584eb2d9
emperorlu <1021779409@qq.com>
2019-10-31 16:28:14 +0800

    aep

--- src/aep_system.cc
+++ src/aep_system.cc
@@ -306,7 +306,7 @@ void aepsystem::Insert(const string &key, const string &value)
     m_mutex.lock();
     // std::lock_guard<std::mutex> lk(m_mutex);
     insert_count++;
-    cout << "[DEBUG] Insert (" << insert_count << ") key: " << key << endl;
+    // cout << "[DEBUG] Insert (" << insert_count << ") key: " << key << endl;
     if(id == 0)  // primary aep
     {
         bptree_nvm0->Insert(char8toint64(key.c_str()),value);
