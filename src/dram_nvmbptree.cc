737e1bec46c541ae3300f63a027495093c90af57
emperorlu <1021779409@qq.com>
2019-10-31 10:22:43 +0800

    aep

--- src/dram_nvmbptree.cc
+++ src/dram_nvmbptree.cc
@@ -699,6 +699,7 @@ bool BpTree::Search(string key1, string key2, string* result, int& size)
 // }
 vector<string> BpTree::OutdeData(size_t out){
     vector<string> dlist;
+    HCrchain->traver();
     for(int i = 0; i < HCrchain->theLists.size(); i++)
     {
         typename list<string>::iterator itr = HCrchain->theLists[i].begin();
