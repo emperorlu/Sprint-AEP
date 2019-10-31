c847a2271b3b018c0a3bed156a633b4af4a35c12
emperorlu <1021779409@qq.com>
2019-10-31 14:41:22 +0800

    aep

--- src/con_btree.h
+++ src/con_btree.h
@@ -236,7 +236,7 @@ class CONRangChain
         {
             typename list<entry_key_t>::iterator itr = theLists[i].begin();
             while(itr != theLists[i].end()){
-                int res = memcmp(x.c_str(), (*itr).c_str(), NVM_KeySize);
+                int res = memcmp(x.c_str(), (*itr).key, NVM_KeySize);
                 if (res == 0){
                     (*itr).sign = '0';
                     return;
