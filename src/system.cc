7b7f51ed4985330f97f8f40008b75c025793088f
emperorlu <1021779409@qq.com>
2019-11-28 15:47:31 +0800

    aep

--- src/system.cc
+++ src/system.cc
@@ -18,10 +18,9 @@ int main(int argc, char **argv)
     size_t VALUE_SIZE = rocksdb::NVM_ValueSize;
     int i;
     int ops = 1000000 * num_size;
-    db_->num_size = ops;
-    db_->is_cache = to_cache;
     db_ = new rocksdb::aepsystem;
     db_->num_size = ops;
+    db_->is_cache = to_cache;
     db_->Initialize();
     char keybuf[KEY_SIZE + 1];
     char valuebuf[VALUE_SIZE + 1];
