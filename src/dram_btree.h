67a65a434826c6cf39e8cc3eee0d9f6061758491
emperorlu <1021779409@qq.com>
2019-12-04 21:02:03 +0800

    aep

--- src/dram_btree.h
+++ src/dram_btree.h
@@ -125,20 +125,20 @@ public:
             case REQ_FLUSH:
                 gettimeofday(&nbe, NULL);
                 insertData = bt->range_leafs();
-                // {
-                //     unique_lock<mutex> lk(r->req_mutex);
-                //     r->finished = true;
-                //     r->signal.notify_one();
-                // }
-                // FlushtoNvm();
                 {
-                    thread f(&RAMBtree::FlushtoNvm, this);
-                    f.join();
+                    unique_lock<mutex> lk(r->req_mutex);
+                    r->finished = true;
+                    r->signal.notify_one();
                 }
+                FlushtoNvm();
+                // {
+                //     thread f(&RAMBtree::FlushtoNvm, this);
+                //     f.join();
+                // }
                 gettimeofday(&nen, NULL);
                 ftime += (nen.tv_sec-nbe.tv_sec) + (nen.tv_usec-nbe.tv_usec)/1000000.0;
-                // return;
-                break;
+                return;
+                // break;
             case REQ_OUT:
                 gettimeofday(&nbe, NULL);
                 r->outdata = OutdeData(r->out);
