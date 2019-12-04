009273f6a30a7dd788d47dfc99d851790df38753
emperorlu <1021779409@qq.com>
2019-12-04 17:10:42 +0800

    aep

--- src/dram_btree.h
+++ src/dram_btree.h
@@ -58,7 +58,7 @@ public:
     int MinHot(){
         return bt->minHot();
     }
-    void NvmInsert(void * arg){
+    void* NvmInsert(void * arg){
         for(int i=0;i<insertData.size();i++){
             bptree_nvm->Insert(insertData[i].key.key, insertData[i].key.hot, string(insertData[i].ptr, NVM_ValueSize));
             current_num ++;
