69459a294be5f01b0b23aa3b6548998347cb7d6a
emperorlu <1021779409@qq.com>
2019-12-09 16:58:14 +0800

    aep

--- rm.sh
+++ rm.sh
@@ -5,3 +5,9 @@ rm -rf /pmem2/datastruct/*
 rm -rf /pmem3/datastruct/*
 
 rm -rf /pmem0/cache/*
+rm -rf /pmem3/cache/*
+
+rm -rf /pmem0/test/*
+rm -rf /pmem1/test/*
+rm -rf /pmem2/test/*
+rm -rf /pmem3/test/*
