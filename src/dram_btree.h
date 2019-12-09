a485ffc84368994e5d46bfc75e703e336f317c7a
emperorlu <1021779409@qq.com>
2019-12-09 12:39:56 +0800

    aep

--- src/dram_btree.h
+++ src/dram_btree.h
@@ -65,6 +65,7 @@ public:
         // vector<ram_entry> insertData = bt->range_leafs();
         while(flush){
             if(insertData.size()!=0){
+                cout << "flush!" << endl;
                 for(int i=0;i<insertData.size();i++){
                     bptree_nvm->Insert(insertData[i].key.key, insertData[i].key.hot, string(insertData[i].ptr, NVM_ValueSize));
                     current_num ++;
