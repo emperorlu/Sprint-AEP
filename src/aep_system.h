086a52736094ad2004d4e3f7026f74837fbef71f
emperorlu <1021779409@qq.com>
2019-12-17 16:26:38 +0800

    aep

--- src/aep_system.h
+++ src/aep_system.h
@@ -25,9 +25,9 @@
 
 
 #define CACHE0  "/pmem0/cache/daram0"
-#define CACHE1  "/pmem2/cache/daram1"
+#define CACHE1  "/pmem0/cache/daram1"
 #define CACHE2  "/pmem0/cache/daram2"
-#define CACHE3  "/pmem0/cache/daram3"
+#define CACHE3  "/pmem2/cache/daram3"
 
 // #define CPATH0  "/pmem0/cache/path0"
 // #define CPATH1  "/pmem0/cache/path1"
